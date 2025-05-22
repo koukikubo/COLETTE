6a6dbc8150f7e4e8446e88d94f352224cf5e8b24
koukikubo <kouki.xx.0817@gmail.com>
2025-05-22 15:36:18 +0900

    ✨ TOPページ（ベース）の作成

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,72 +1,22 @@
-"use client";
-import React, { useEffect, useState } from "react";
-
-type Post = {
-  id: number;
-  title: string;
-};
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
 
 export default function Home() {
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [newTitle, setNewTitle] = useState("");
+  const cookieStore = cookies();
+  const token = cookieStore.get("access-token")?.value;
+  const client = cookieStore.get("client")?.value;
+  const uid = cookieStore.get("uid")?.value;
 
-  const fetchPosts = async () => {
-    try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/posts`);
-      if (!response.ok) {
-        throw new Error("データの取得に失敗しました");
-      }
-      const data = await response.json();
-      setPosts(data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  useEffect(() => {
-    fetchPosts();
-  }, []);
-
-  const handleSubmit = async (event: React.FormEvent) => {
-    event.preventDefault();
-    try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/posts`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ title: newTitle }),
-      });
-      if (!response.ok) {
-        throw new Error("投稿に失敗しました");
-      }
-      setNewTitle("");
-      fetchPosts();
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  if (!token || !client || !uid) {
+    redirect("auth/signup"); // 新規登録ページへ
+  }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h2 className="text-3xl mb-4">記事の一覧</h2>
-      <form onSubmit={handleSubmit} className="mt-4 mb-4">
-        <input
-          type="text"
-          value={newTitle}
-          onChange={(e) => setNewTitle(e.target.value)}
-          placeholder="新しい投稿のタイトル"
-          className="mr-2 p-2 border"
-        />
-        <button type="submit" className="p-2 bg-blue-500 text-white">
-          投稿する
-        </button>
-      </form>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>{post.title}</li>
-        ))}
-      </ul>
-    </main>
+    <div>
+      <h1 className="text-xl font-bold">COLETTE TOPページ</h1>
+      <p className="text-gray-600 mt-2">
+        ここに予約一覧やお知らせなどが表示されます。
+      </p>
+    </div>
   );
 }
