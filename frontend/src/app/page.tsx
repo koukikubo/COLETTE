84b6089fbefde490264f3e3393741cf41edcaa7b
koukikubo <kouki.xx.0817@gmail.com>
2025-07-07 19:34:08 +0900

    🎨 Cookieを使用するため、一時的にコメントアウト

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,15 @@
-import { redirect } from "next/navigation";
-import { cookies } from "next/headers";
+// import { redirect } from "next/navigation";
+// import { cookies } from "next/headers";
 
 export default function Home() {
-  const cookieStore = cookies();
-  const token = cookieStore.get("access-token")?.value;
-  const client = cookieStore.get("client")?.value;
-  const uid = cookieStore.get("uid")?.value;
+  // const cookieStore = cookies();
+  // const token = cookieStore.get("access-token")?.value;
+  // const client = cookieStore.get("client")?.value;
+  // const uid = cookieStore.get("uid")?.value;
 
-  if (!token || !client || !uid) {
-    redirect("auth/signup"); // 新規登録ページへ
-  }
+  // if (!token || !client || !uid) {
+  //   redirect("auth/signup"); // 新規登録ページへ
+  // }
 
   return (
     <div>
